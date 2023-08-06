07fb8b532d68a33e3077e2bea1d51f4e87da2528
Rom Ihia <118122586+Romihia@users.noreply.github.com>
2023-08-06 19:46:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import Restrictions
 import os
 
 
-# rest = Restriction(
-    # filename = "Right_hand_dumbbell_lift.json" , 
-    # landmarks = PoseDetection().detect_pose(image_path="training-with-focus.jpg" , display=False)
-    # )
-#
-play=Pose_detection.PoseDetection()
+#Pose Detection on Images
+pose_detector = PoseDetection()
+image_path = "path/to/your/image.jpg"
+landmarks = pose_detector.detect_pose(image_path, display=True)
 
+
+#Live Pose Detection
+
+play=Pose_detection.PoseDetection()
 play.detect_pose_live(filename = "deadlift.json")
 
-#  rest = Restriction( filename , image_landmarks_results)
-#   title = rest.get_restrictions()
-#   print(list(rest.get_restrictions()['angle']))
+
 
